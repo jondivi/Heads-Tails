b442820016e09352a712b9551a5c537e7b528e71
Jon Divi <98039686+jondivi@users.noreply.github.com>
2024-01-23 15:02:21 -0500

    updates

--- main.js
+++ main.js
@@ -8,23 +8,28 @@ let x = 0
 function hideAndRunHeads(){
     document.querySelector('#heads').style.display = 'none';
 	document.querySelector('#tails').style.display = 'none';
+    
     x = Math.floor(Math.random() * 2);
-
     if(x === 0){
-        document.querySelector('#result').innerText='Tails Wins'
+        document.querySelector('#result').innerText='Tails Wins';
+        document.querySelector('.hidden').classList.toggle('hidden')
     }else{
-        document.querySelector('#result').innerText='Heads Wins'
+        document.querySelector('#result').innerText='Heads Wins';
+        document.querySelector('.hidden').classList.toggle('hidden')
     }}
 
 function hideAndRunTails(){
     document.querySelector('#heads').style.display = 'none';
 	document.querySelector('#tails').style.display = 'none';
-    x = Math.floor(Math.random() * 2);
 
+    x = Math.floor(Math.random() * 2);
     if(x === 0){
-        document.querySelector('#result').innerText='Tails Wins'
+        document.querySelector('#result').innerText='Tails Wins';
+        document.querySelector('.hidden').classList.toggle('hidden')
+
     }else{
-        document.querySelector('#result').innerText='Heads Wins'
+        document.querySelector('#result').innerText='Heads Wins';
+        document.querySelector('.hidden').classList.toggle('hidden')
     }
 }
 
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 A small HTML, CSS, and JS program for picking heads or tails
+
+## Optimizations
+
+- Put together a layout
+- Add styling
+
+## Additions
+
+- Add an option to use different currency (i.e. Default will be a US Quarter, but there could be Euros, Pounds, Yen, etc.)
